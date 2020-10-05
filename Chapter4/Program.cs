f667fa64d899956ff57fd08dd9a1054fe9efa62e
YataboriHijiri <30056@ojs.ac.jp>
2020-10-05 15:07:00 +0900

    chapter５

--- Chapter4/Program.cs
+++ Chapter4/Program.cs
@@ -9,29 +9,65 @@ namespace Chapter4
 {
     class Program 
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(GetProduct());
-        }
+        static void Main(string[] args) {
 
-        private static string GetProduct() 
-        {
-            Sale sale = new Sale {
-                ShopName = "pet store",
-                Amout = 10000,
-                Product = "food"
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
             };
 
-            sale = null;
-            return sale?.Product;
-        }
-    }
+            // 4.2.2(呼び出し)
+            Console.WriteLine("-- 4.2.2 --");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("-----------\n");
 
-    class Sale {
-        //店舗名
-        public string ShopName { get; set; }
-        //売上高
-        public int Amout { get; set; }
-        public string Product { get; set; }
+            // 4.2.4(呼び出し)
+            Console.WriteLine("-- 4.2.4 --");
+            Exercise2_4(ymCollection);
+            Console.WriteLine("-----------\n");
+
+            // 4.2.5(呼び出し)
+            Console.WriteLine("-- 4.2.5 --");
+            Exercise2_5(ymCollection);
+            Console.WriteLine("-----------\n");
+        }
+        // 4.2.2
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach(var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+        // 4.2.3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach(var ym in yms) {
+                if(ym.Is21Century)
+                    return ym;
+            }
+            return null;
+        }
+        // 4.2.4
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var yearmonth = FindFirst21C(ymCollection);
+#if false
+            var s = yearmonth == null ? "21世紀のデータはありません" : yearmonth.ToString();
+            Console.WriteLine(s);
+#else
+            if(yearmonth == null)
+                Console.WriteLine("21世紀のデータはありません");
+            else
+                Console.WriteLine(yearmonth);
+#endif
+        }
+        // 4.2.5
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach(var ym in array) {
+                Console.WriteLine(ym);
+            }
+        } 
     }
 }
---
+++ Chapter5/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter5 
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            var lines = "Novelist=谷崎純一郎;BestWork=春琴沙;Born=1886";
+
+            #region //5.1
+            //5.1
+            Console.WriteLine("-----5.1--------");
+            Console.Write("文字列1 :");
+            var s1 = Console.ReadLine();
+
+            Console.Write("文字列2 : ");
+            var s2 = Console.ReadLine();
+            if(String.Compare(s1, s2, ignoreCase: true) == 0) 
+            {
+                Console.WriteLine("等しい");
+            } 
+            else 
+            {
+                Console.WriteLine("等しくない");
+            }
+            #endregion
+
+
+            #region//5.2
+            //5.2
+            Console.WriteLine("-----5.2--------");
+            Console.Write("数値文字列 : ");
+            var line = Console.ReadLine();
+
+            int num;  //変換後の数値格納用
+
+            if(int.TryParse(line,out num)) {
+                //Console.WriteLine(num.ToString("#,#"));
+                //Console.WriteLine("{#,#", num);
+                Console.WriteLine($"{num:#.,#}");
+            } else {
+                Console.WriteLine("数値文字列ではありません");
+            }
+            #endregion
+
+
+
+            #region//5.3
+            //5.3.1
+            Console.WriteLine("-----5.3.1--------");
+            int spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
+
+            //5.3.2
+            Console.WriteLine("-----5.3.2--------");
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+
+            //5.3.3
+            Console.WriteLine("-----5.3.3--------");
+            int count = text.Split(' ').Count();
+            Console.WriteLine("単語数;{0}", count);
+
+
+            //5.3.4
+            Console.WriteLine("-----5.3.4--------");
+            text.Split(' ')
+                 .Where(s => s.Length <= 4)
+                 .ToList()
+                 .ForEach(Console.WriteLine);
+
+            //5.3.5
+            Console.WriteLine("-----5.3.5--------");
+            var array = text.Split( ' ')
+                .ToArray();
+            #endregion
+
+
+            //5.4
+            Console.WriteLine("-----5.4--------");
+            foreach(var item in lines.Split(';')) {
+                var word = item.Split('=');
+                Console.WriteLine("{0}:{1}", ToJapanese(word[0]), word[1]);
+            }
+        }
+        static string ToJapanese(string key) {
+            switch(key) {
+                case "Novelist":
+                    return "作家";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+                default:
+                    return "    ";
+
+            }
+
+
+        }
+
+
+
+    }
+}
