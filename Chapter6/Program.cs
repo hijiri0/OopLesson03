5eddacad33e6c80002a4cce3fa3b2b90339f795f
YataboriHijiri <30056@ojs.ac.jp>
2020-10-15 10:02:17 +0900

    教科書P196まで

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -12,60 +12,48 @@ namespace Chapter6
      {
         static void Main(string[] args) 
         {
-            var numbers = new List<int> { 9, 7, -5, 4, 2, 5, 4, 2, -4, 8, -1,6,4 };
-            Console.WriteLine($"平均値:{numbers.Average()}");
-            Console.WriteLine($"合計値:{numbers.Sum()}");
-            Console.WriteLine($"最小値:{numbers.Where(n=>n>0).Min()}");
-            Console.WriteLine($"最大値:{numbers.Max()}");
+            //整数の例
+            var numbers = new List<int> { 19, 17, 15, 24, 12, 25, 14, 20, 12, 28, 19, 30, 24 };
 
-            bool exists = numbers.Any(n => n % 7 == 0);
+            var strings = numbers.Distinct().ToArray();
+            foreach(var str in strings) {
+                Console.Write(str + " ");
+            }
 
-            var results = numbers.Where(n => n > 0).Take(5);
-            foreach(var result in results) {
-                Console.Write(result + " ");
+            Console.WriteLine();//改行
+            numbers.Distinct().Select(n => n.ToString("0000")).ToList().ForEach(s => Console.Write(s + " "));
+
+            //並べ替え
+            Console.WriteLine();//改行
+            var sortedNumbers = numbers.OrderBy(n=>n);
+            foreach(var nums in sortedNumbers) {
+                Console.Write(nums + " ");
             }
 
 
+            //文字列の例
+            Console.WriteLine("\n\n----------------");
+            var words = new List<string> {
+              "Microsoft","Apple","Google","Oracle","Facebook",
+            };
 
+            var lower = words.Select(name => name.ToLower()).ToArray();
+
+
+            //Bookオブジェクト
+            Console.WriteLine("\n\n----------------");
             var books = Books.GetBooks();
-            Console.WriteLine($"本の平均価格:{ books.Average(x =>x.Price)}");
-            Console.WriteLine($"本の合計価格:{ books.Sum(x => x.Price)}");
-            Console.WriteLine($"本のページが最大:{ books.Average(x => x.Price)}");
-            Console.WriteLine($"高価な本:{ books.Average(x => x.Price)}");
-            Console.WriteLine($"タイトルに「物語」がある冊数:{books.Where(x => x.Title == "物語").Count()}");
-
-            //600ページを超える書籍があるか？
-            Console.WriteLine("600ページを超える書籍は");
-            Console.WriteLine(books.Any(x => x.Pages > 600) ? "ある" : "ない");
-
-            #region//
-            //bool over = books.Any(x => x.Pages >= 600);
-            //if(true) 
-            //{
-            //    Console.WriteLine("あります");
-            //}    
-            //else {
-            //    Console.WriteLine("ありません");
-            //}
-            #endregion
-            //すべてが200ページ以上の書籍か？(Allメソッド)
-            Console.Write("200ページ以上の書籍は");
-            Console.WriteLine(books.All(x => x.Pages >= 200)?"あります":"ありません");
-
-            //400ページを超える本は何冊目か?
-            var book = books.FirstOrDefault(x => x.Pages > 400);
-            int i;
-            for( i = 0; i < books.Count; i++) {
-                if(books[i].Title.Contains(book.Title)) {
-                    break;
-                }
+            //タイトルリスト
+            var titles = books.Select(name =>name.Title);
+            foreach(var title in titles) {
+                Console.Write(title + " ");
             }
-            Console.WriteLine($"400ページを超える本は{i+1}冊目です");
 
-            //本の値段が400円以上のものを3冊表示
-            var bok = books.Where(x => x.Price >= 400);
-            foreach(var item in bok) {
-                Console.WriteLine(item.Title + " ");
+
+            //ページ数の多い順に並べ替え(または金額の高い順)
+            var sortedBooks = books.OrderByDescending(book => book.Pages);
+            foreach(var book in sortedBooks) {
+                Console.WriteLine(book.Title + " " + book.Pages);
             }
 
 
---
+++ Chapter7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter7 
+    {
+    class Program 
+    {
+        static void Main(string[] args) 
+       {
+
+
+            DuplicateKeySample();
+       }
+        static void DuplicateKeySample() {
+            // ディクショナリの初期化
+            var dict = new Dictionary<string, List<string>>() {
+               { "PC", new List<string> { "パーソナル コンピュータ", "プログラム カウンタ", } },
+               { "CD", new List<string> { "コンパクト ディスク", "キャッシュ ディスペンサー", } },
+            };
+
+            // ディクショナリに追加
+            var key = "EC";
+            var value = "電子商取引";
+            if(dict.ContainsKey(key)) {
+                dict[key].Add(value);
+            } else {
+                dict[key] = new List<string> { value };
+            }
+
+            // ディクショナリの内容を列挙
+            foreach(var item in dict) {
+                foreach(var term in item.Value) {
+                    Console.WriteLine("{0} : {1}", item.Key, term);
+                }
+            }
+        }
+
+
+    }
+}
